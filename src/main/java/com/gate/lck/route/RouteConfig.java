cd825cb983f80ac4b3ed75f9a43155033d5fc415
beomshin.kim <chzh859@naver.com>
2022-12-03 10:47:49 +0900

    # route , filter 추가

---
+++ src/main/java/com/gate/lck/route/RouteConfig.java
@@ -0,0 +1,37 @@
+package com.gate.lck.route;
+
+import com.gate.lck.filter.LoggingFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator userRoute(RouteLocatorBuilder builder, LoggingFilter httpLoginFilter) {
+        return builder.routes()
+                .route("lck-user-service", p -> p.path("/lck-user-service/**")
+                        .filters(f ->
+                                f.filter(
+                                        httpLoginFilter
+                                                .apply(new LoggingFilter.Config("lck-user-serviceLoggingFilter", true, true))))
+                        .uri("lb://LCK-USER-SERVICE"))
+                .build();
+    }
+
+
+    @Bean
+    public RouteLocator trialRoute(RouteLocatorBuilder builder, LoggingFilter httpLoginFilter) {
+        return builder.routes()
+                .route("lck-trial-service", p -> p.path("/lck-trial-service/**")
+                        .filters(f ->
+                                f.filter(
+                                        httpLoginFilter
+                                                .apply(new LoggingFilter.Config("lck-trial-service LoggingFilter", true, true))))
+                        .uri("lb://LCK-TRIAL-SERVICE"))
+                .build();
+    }
+
+}
